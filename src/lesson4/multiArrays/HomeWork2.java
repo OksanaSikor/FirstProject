15a5dd3290dde2eb7deb51b9e86c79763d8acf28
OSikor <OSikor@lanit.ru>
2023-07-10 18:10:04 +0300

    lesson 4 homework(многомерные массивы)

--- src/lesson4/multiArrays/HomeWork2.java
+++ src/lesson4/multiArrays/HomeWork2.java
@@ -1,5 +1,7 @@
 package lesson4.multiArrays;
 
+import java.util.Arrays;
+
 /**
  * Умножение двух матриц
  * Создайте два массива целых чисел размером 3х3 (две матрицы).
@@ -10,4 +12,35 @@ package lesson4.multiArrays;
  */
 
 public class HomeWork2 {
+    public static void main(String[] args) {
+
+
+        int[][] matrixA = {{1, 0, 0, 0},
+                {0, 1, 0, 0},
+                {0, 0, 0, 0}};
+
+        int[][] matrixB = {{1, 2, 3},
+                {1, 1, 1},
+                {0, 0, 0},
+                {2, 1, 0}};
+        int matrixC[][] = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int u = 0; u < 3; u++) {
+                for (int j = 0; j < 4; j++) {
+
+                    matrixC[i][u] += matrixA[i][j] * matrixB[j][u];
+                }
+            }
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrixB[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+    }
 }
--- src/lesson4/multiArrays/HomeWork3.java
+++ src/lesson4/multiArrays/HomeWork3.java
@@ -5,4 +5,24 @@ package lesson4.multiArrays;
  */
 
 public class HomeWork3 {
+    public static void main(String[] args) {
+        int a[][] = new int[5][5];
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                a[i][j] = (int) (Math.random() * 40);
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int sum = 0;
+        for(int i = 0;i < 5;i++) {
+            for (int j = 0; j < 5; j++) {
+                sum += a[i][j];
+            }
+        }
+                System.out.println("Сумма всех элементов массива - " + sum);
+    }
 }
+
+
